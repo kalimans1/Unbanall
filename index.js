b3a3a99e85ceaa3bd6cc4f8fa8d8c08ca838a7a6
kalimans1 <kanadalipes@gmail.com>
2024-08-06 19:05:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ```
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'YOUR_BOT_TOKEN';
+const token = 'process.env.token';
 const prefix = '.';
 
 client.on('ready', () => {
