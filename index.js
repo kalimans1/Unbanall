5a1d6d82659a3ba673295fe0d5e2f9c467dda9ea
pamiradasilva <125839294+pamiradasilva@users.noreply.github.com>
2024-08-06 15:33:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'YOUR_BOT_TOKEN';
+const prefix = '.';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(`${prefix}unbanall`)) {
+        if (!message.guild) return message.reply('This command can only be used in a server.');
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You do not have permission to unban members.');
+
+        message.guild.fetchBans()
+            .then(bans => {
+                if (bans.size === 0) return message.reply('There are no banned users.');
+
+                bans.forEach(ban => {
+                    message.guild.unban(ban.user.id)
+                        .then(() => {
+                            message.channel.send(`Unbanned ${ban.user.tag}`);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            message.channel.send(`Failed to unban ${ban.user.tag}`);
+                        });
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                message.reply('Failed to fetch bans.');
+            });
+    }
+});
+
+client.login(token);
